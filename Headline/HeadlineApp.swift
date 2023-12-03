d87d8cdae236818ae392de028c0d26c39a5a193e
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-12-03 21:09:22 +0200

    Initial Commit

---
+++ Headline/HeadlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadlineApp.swift
+//  Headline
+//
+//  Created by Pavel Semenchenko on 03.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeadlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
