d184e0ca2c0cde705e544a7f59b85a0c2e4c41b0
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-12-03 21:50:42 +0200

    init

---
+++ Headline/NavigationRouter.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationRouter.swift
+//  Headline
+//
+//  Created by Pavel Semenchenko on 03.12.2023.
+//
+
+import SwiftUI
+
+struct NavigationRouter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationRouter()
+}
---
+++ Headline/TabBar.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Headline
+//
+//  Created by Pavel Semenchenko on 03.12.2023.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            NavigationStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    TabBar()
+}
