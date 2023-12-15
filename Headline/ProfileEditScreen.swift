685fa33196dcc277e2ff435e6e7054fa25d30f2a
Leksovich <imagemakerspa@gmail.com>
2023-12-15 18:58:00 +0200

    ui

---
+++ Headline/ProfileEditScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditScreen.swift
+//  Headline
+//
+//  Created by Pavel Semenchenko on 15.12.2023.
+//
+
+import SwiftUI
+
+struct ProfileEditScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditScreen()
+}
